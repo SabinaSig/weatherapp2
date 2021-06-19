034156dae3bd5234853b8db03630f2671b89581c
SabinaSig <77678314+SabinaSig@users.noreply.github.com>
2021-06-19 22:54:38 +0200

    Creating new compotents

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,6 +1,7 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon.js";
+import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo (props) {
     return (
@@ -10,46 +11,47 @@ export default function WeatherInfo (props) {
         <div id="current">
           <p>
             <span>
+              <FormattedDate date={props.data.date} />
+            
+            <hr></hr>
+            </span>
+            
+            <span>
+            <strong> {props.data.city}  </strong>
+            </span>
+          
+            <p>
+              <span className="text-capitalize"> <strong>{props.data.description}</strong>  </span>
+              <br></br>
+            <span>
               {" "}
               <WeatherIcon code={props.data.icon} />
               {" "}
             </span>
-            <br />
-            <span>
-              <FormattedDate date={props.data.date} />
-            </span>
-            <br />
-            <strong> {props.data.city}  </strong>
-            <p>
-              <span><strong className="temperature"> {Math.round(props.data.temperature)} </strong> </span>
-              <span id="degree"> °C </span>
+            <br></br>
+            <WeatherTemperature celsius={props.data.temperature} />
             </p>
-
-            <p className="currentState">
-              <ul>
-                <li>
-                  <span className="text-capitalize"> <strong>{props.data.description}</strong>  </span>
-                </li>
-                <li id="pressure"> {props.data.precipitation} </li>
-                <li>
-                  {" "}
-                   Humidity (%):{" "}
-                </li>{" "}
-                <li id="humidity"> {props.data.humidity} </li>
-                <li>
-                  {" "}
-                  Wind (m/s):{" "}
-                </li>{" "}
-                <li id="wind"> {props.data.wind} </li>
-              </ul>
-            </p>
-
             <p id="change">
               {" "}
               Change to
               <button id="farenheit"> °F </button> |
               <button id="celsius"> °C </button>
             </p>
+
+            <p className="currentState">
+              <ul>
+                <li id="pressure"> {props.data.precipitation} </li>
+                <li id="humidity">
+                  {" "}
+                   Humidity:{" "} {props.data.humidity} %
+                </li>{" "}
+                <li id="wind">
+                  {" "}
+                  Wind: {props.data.wind} m/s
+                </li>{" "}
+              </ul>
+            </p>
+
           </p>
         </div>
       </div>
